4682ccbe62200fc06db4e0b3429bd14a912e36b6
hengxiao <hengxiao@iflytek.com>
2022-09-07 23:19:31 +0800

    🎈 perf(axios): TS泛型

--- src/apis/test.ts
+++ src/apis/test.ts
@@ -1,3 +1,3 @@
 import { post } from './index'
 
-export const getTestData = (params = {}) => post('mock/test.json', params)
+export const getTestData = (params = {}) => post<MOCK.STOCk>('mock/test.json', params)
--- src/types/mock.d.ts
+++ src/types/mock.d.ts
@@ -1,35 +1,20 @@
 declare namespace MOCK {
-  interface YILI_ITEM {
-    situation: string,
-    how: string,
-    premise: string,
-    why: string,
-    or: string,
-    story: string,
-    note: string,
+  interface STOCk_BASE_ITEM {
+    id: string
+    area: string
+    name: string
+    price: string
+    time: number
+    note?: string
+  }
+  interface STOCk_SUB_ITEM {
+    color: string
+    size: string
   }
 
-  interface GUA {
-    guaXu: number,
-    guaMing: string,
-    guaXiang: string,
-    daXiang: string,
-    guaCi: string,
-    yaoCi: string [],
-    yiXiang: string,
-    yiLi: YILI_ITEM [],
-    leiXiang: string,
-    yaoXiang: string [],
-    baGong: string,
-    baGongOrder: 10,
-    chongHe: string,
-    naJia: string [],
-    shiYing: {
-      [key: number]: string
-    },
-  }
 
-  type ZHOUYI = GUA []
+  type STOCk_ITEM = STOCk_BASE_ITEM & STOCk_SUB_ITEM
+  type STOCk = STOCk_ITEM []
 }
 
 
