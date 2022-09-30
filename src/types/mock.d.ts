18b84051aca2b4e05fe70dcb5e8e71509d97fb4e
hengxiao <hengxiao@iflytek.com>
2022-09-30 21:57:32 +0800

    [修改代码] 文件类型

--- src/types/mock.d.ts
+++ src/types/mock.d.ts
@@ -1,18 +1,9 @@
 // !export default ZHOUYI：默认导出
-// declare module '@/mock/ZHOUYI' {
-//   const ZHOUYI: MOCK.ZHOUYI
-//   export = ZHOUYI
-// }
+declare module '@/mock/ZHOUYI' {
+  const ZHOUYI: MOCK.ZHOUYI
+  export = ZHOUYI
+}
 
-// !export const LUNYU：模块导出
-// declare module '@/mock/lunyu' {
-//   export const LUNYU: MOCK.ZHOUYI
-// }
-
-// !export default ZHOUYI：默认导出批量处理
-// declare module '@/mock/*' {
-
-// }
 declare module '@/mock/*' {
 
 }
