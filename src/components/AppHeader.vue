e5477c86c28f6b83440183b73132a59385619399
hengxiao <hengxiao@iflytek.com>
2023-02-18 19:20:38 +0800

    🎉 init(header): 控制返回路径 && 增加宽度限制

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -6,15 +6,19 @@ const { title } = toRefs(useHeaderStore())
 const router = useRouter()
 const route = useRoute()
 const emitter = inject('emitter') as Emitter<{ 'on-click-right': unknown }>
+const toPath = ref('')
 const leftArrow = ref(false)
 const rightText = ref('')
+const isSlot = ref(false)
 
 useHead({ title })
 
 watch(() => route.meta, (meta) => {
+  toPath.value = meta.toPath as string
   title.value = meta.title as string
   leftArrow.value = meta.leftArrow as boolean
   rightText.value = meta.rightText as string
+  isSlot.value = meta.isPlain as boolean
 }, { immediate: true })
 
 function onClickRight() {
@@ -28,6 +32,12 @@ function onClickRight() {
     :left-arrow="leftArrow"
     :right-text="rightText"
     @click-right="onClickRight"
-    @click-left="router.go(-1)"
-  />
+    @click-left="toPath ? router.push(toPath) : router.go(-1)"
+  >
+    <template v-if="isSlot" #right>
+      <p w-4em rounded-full border-base border-pramiry p-2 text-sm scale-75>
+        {{ rightText }}
+      </p>
+    </template>
+  </VanNavBar>
 </template>
