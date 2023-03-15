75e7ea69b8bd098b14ca624cc0d19b7df48e5ae5
hengxiao <hengxiao@iflytek.com>
2023-03-15 23:42:54 +0800

    📃 docs(build): 增加预渲染

--- vite.build.ts
+++ vite.build.ts
@@ -3,31 +3,40 @@ import type { BuildOptions } from 'vite'
 const nameREG = /([_])[.]{3}([^.]+)\1(.css)?/
 const replace = (template: string, value: string) => template.replace(/\[name\]/, value.replace(nameREG, '$2'))
 
-const buildOptions: BuildOptions = {
-  rollupOptions: {
-    output: {
-      // https://rollupjs.org/guide/en/#outputassetfilenames
-      assetFileNames: (info) => {
-        const template = 'assets/[name].[hash][extname]'
-        if (nameREG.test(info.name)) return replace(template, info.name)
-        return template
-      },
-      // https://rollupjs.org/guide/en/#outputchunkfilenames
-      chunkFileNames: (info) => {
-        const template = '[name].[hash].js'
-        if (nameREG.test(info.name)) return replace(template, info.name)
-        return template
-      },
-      manualChunks: (id) => {
+const getBuild = (VITE_APP_DIST: string): BuildOptions => {
+  const output: BuildOptions['rollupOptions']['output'] = {
+    // https://rollupjs.org/guide/en/#outputassetfilenames
+    assetFileNames: (info) => {
+      const template = 'assets/[name].[hash][extname]'
+      if (nameREG.test(info.name)) return replace(template, info.name)
+      return template
+    },
+    // https://rollupjs.org/guide/en/#outputchunkfilenames
+    chunkFileNames: (info) => {
+      const template = '[name].[hash].js'
+      if (nameREG.test(info.name)) return replace(template, info.name)
+      return template
+    },
+  }
+
+  if (VITE_APP_DIST === 'SMC') {
+    output.manualChunks = (id) => {
+      if (VITE_APP_DIST === 'SMC') {
         if (id.includes('element'))
           return 'element-vendor'
         else if (id.includes('lunar'))
           return 'lunar-vendor'
         else if (id.includes('node_modules'))
           return 'vendor'
-      },
+      }
+    }
+  }
+
+  return {
+    rollupOptions: {
+      output,
     },
-  },
+  }
 }
 
-export default buildOptions
+export default getBuild
