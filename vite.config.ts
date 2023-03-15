300fd6dccf3a12b771808df281de68705be86149
hengxiao <hengxiao@iflytek.com>
2023-03-15 23:42:54 +0800

    📃 docs(build): 增加预渲染

--- vite.config.ts
+++ vite.config.ts
@@ -10,45 +10,55 @@ import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite'
 import { VantResolver } from 'unplugin-vue-components/resolvers'
 import { viteSingleFile } from 'vite-plugin-singlefile'
+import vitePrerender from 'vite-plugin-prerender'
 import CopyPlugin from 'vite-copy-plugin'
 import viteMock from 'vite-plugin-easy-mock'
-import build from './vite.build'
+import getBuild from './vite.build'
 
 // https://vitejs.dev/config/
-export default ({ mode }: ConfigEnv) => defineConfig({
-  base: loadEnv(mode, process.cwd()).VITE_APP_BASE_URL,
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src'), // 设置 `@` 指向 `src` 目录
+export default ({ mode }: ConfigEnv) => {
+  const env = loadEnv(mode, process.cwd())
+  const { VITE_APP_BASE_URL, VITE_APP_DIST } = env
+
+  return defineConfig({
+    base: VITE_APP_BASE_URL,
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, 'src'), // 设置 `@` 指向 `src` 目录
+      },
     },
-  },
-  plugins: [
-    autoImport({
-      imports: ['vue', 'vue-router', '@vueuse/core', '@vueuse/head'],
-      dts: './src/auto-imports.d.ts',
-    }),
-    Components({
-      dts: './src/components.d.ts',
-      resolvers: [VantResolver()],
-    }),
-    // https://www.npmjs.com/package/vite-copy-plugin
-    CopyPlugin([
-      { from: 'config.xml.bak', to: 'dist/config.xml' },
+    plugins: [
+      autoImport({
+        imports: ['vue', 'vue-router', '@vueuse/core', '@vueuse/head'],
+        dts: './src/auto-imports.d.ts',
+      }),
+      Components({
+        dts: './src/components.d.ts',
+        resolvers: [VantResolver()],
+      }),
+      // https://www.npmjs.com/package/vite-copy-plugin
+      CopyPlugin([
+        { from: 'config.xml.bak', to: 'dist/config.xml' },
       // mock里面的文件 =>dist/mock文件夹
       // { from: 'mock', to: 'dist/mock' },
       // { from: 'server.js', to: 'dist/app.js' },
       // { from: 'start.bat', to: 'dist/start.bat' },
-    ]),
-    vue(),
-    pages(),
-    unocss(),
-    viteSingleFile(),
-    viteMock(),
-  ],
-  build,
-  server: {
-    proxy: {
-      '/1.1/classes': 'https://njr7h2zt.lc-cn-n1-shared.com',
+      ]),
+      vue(),
+      pages(),
+      unocss(),
+      VITE_APP_DIST === 'SSF' && viteSingleFile(),
+      VITE_APP_DIST === 'SSG' && vitePrerender({
+        staticDir: resolve(__dirname, 'dist'),
+        routes: ['/', '/gua'],
+      }),
+      viteMock(),
+    ],
+    build: getBuild(VITE_APP_DIST),
+    server: {
+      proxy: {
+        '/1.1/classes': 'https://njr7h2zt.lc-cn-n1-shared.com',
+      },
     },
-  },
-})
+  })
+}
